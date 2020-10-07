367c26a61e0899a82fa90c0db925b9e809424821
Guilherme Pacianotto <pacianotto@ipag.com>
2020-10-07 11:57:33 -0300

    comecei

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './index.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* vazio, por enquanto */}
+            </div>
+        )
+    }
+}
+
+export default App
